02f0f133c67f32e565e8a63143cf4f6e97928322
mauroluciano <mauroluciano911@gmail.com>
2023-08-31 17:02:34 +0200

    add element

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,13 @@ createApp({
   data() {
     return {
       titleToDo: "My To Do List",
+
+      myList: [
+        { text: "Fare la spesa", done: false },
+        { text: "Accendere il PC", done: false },
+        { text: "Seguire la lezione", done: true },
+        { text: "Imprecare", done: false },
+      ],
     };
   },
 }).mount("#app");
